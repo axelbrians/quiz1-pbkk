6a0f076a3f358075cdd4b498268983080cd67ab2
axelbrians-pegipegi <axel.briano@pegipegi.com>
2022-03-07 17:23:28 +0700

    feat: complete homepage, add proportion of product

---
+++ quiz1-pbkk/Pages/q1/Collaboration.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace quiz1_pbkk.Pages.q1;
+
+public class Collaboration : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ quiz1-pbkk/Pages/q1/Product.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace quiz1_pbkk.Pages.q1;
+
+public class Product : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
